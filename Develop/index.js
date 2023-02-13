1ef3026bb47cb20f334ba275c3833e648f4d6594
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-02-13 15:23:25 -0700

    Setup app

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,29 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "Give a description of the project.",
+    "Choose relevant sections for your table of contents.",
+    "Provide steps for installation.",
+    "Provide steps for usage.",
+    "Provide credits.",
+    "What license did you use for the project?",
+    "Provide relevant badges.",
+    "List or describe features.",
+    "Explain how to contribute.",
+    "Provide any relevant tests that can be performed.",
+    // Additional
+    "What is your GitHub username?",
+    "What is your email?"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
